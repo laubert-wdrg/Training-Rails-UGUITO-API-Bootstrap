86379ae40e9f62da5795e87a32c41b99cdba0a35
laubert-wdrg <lautaro.aubert@widergy.com>
2026-03-17 16:00:14 -0300

    feat: test notes

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :note do
     title { "MyString" }
     content { "MyText" }
-    note_type { 1 }
-    user { nil }
+    note_type { :critique }
+    user { association :user, utility: create(:north_utility) }
   end
 end
