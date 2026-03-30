ec816b104e19341aa39fa068f0c5dfc0877f0f0d
laubert-wdrg <lautaro.aubert@widergy.com>
2026-03-30 14:42:26 -0300

    fix: n+1

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -16,7 +16,7 @@ module Api
       private
 
       def notes
-        @notes = Note.all
+        @notes = Note.includes(:utility)
         @notes = @notes.where(note_type: params[:type]) if params[:type].present?
         @notes = @notes.order(created_at: params[:order].presence || :desc)
       end
